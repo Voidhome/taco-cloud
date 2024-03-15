b728ea0c84b601ce7b5c6c567cb93d07273f180b
voidhome <ego.zero.inc@gmail.com>
2024-03-15 23:42:01 +0300

    feat: Add Spring Security dependency, updated configurations and modified views

--- src/main/java/tacos/config/DataLoader.java
+++ src/main/java/tacos/config/DataLoader.java
@@ -3,15 +3,19 @@ package tacos.config;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import tacos.entity.Ingredient;
 import tacos.entity.Type;
+import tacos.entity.User;
 import tacos.repository.IngredientRepository;
+import tacos.repository.UserRepository;
 
 @Configuration
 public class DataLoader {
 
     @Bean
-    public CommandLineRunner run (IngredientRepository ingredientRepository){
+    public CommandLineRunner run (IngredientRepository ingredientRepository, UserRepository userRepository,
+                                  PasswordEncoder encoder){
         return args -> {
             ingredientRepository.save(new Ingredient("FLTO", "Flour Tortilla", Type.WRAP));
             ingredientRepository.save(new Ingredient("COTO", "Corn Tortilla", Type.WRAP));
@@ -23,6 +27,9 @@ public class DataLoader {
             ingredientRepository.save(new Ingredient("JACK", "Monterrey Jack", Type.CHEESE));
             ingredientRepository.save(new Ingredient("SLSA", "Salsa", Type.SAUCE));
             ingredientRepository.save(new Ingredient("SRCR", "Sour Cream", Type.SAUCE));
+
+            userRepository.save(new User("user", encoder.encode("user"), "Test User",
+                    "Test street", "Test city", "Test state", "Test zip", "Test phone"));
         };
     }
 }
